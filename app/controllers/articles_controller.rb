4d618d3784798245b8a2130a1eb72ed7bbe64221
sato-koi <sato.koichi.0404@gmail.com>
2018-12-05 20:11:51 +0900

    アプリ完成版

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,32 +8,28 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-
   def create
     @article = Article.new(article_params)
     @article.save
     redirect_to @article
   end  
-
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to @article
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+  
   private
 
   def article_params
     params.require(:article).permit(:title, :body)
   end  
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def update
-    @article = Article.find(params[:id])
-    @article.update(item_params)
-    redirect_to "/articles/#{@article.id}"
-  end
-
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    redirect_to items_path
-  end
-
 end
